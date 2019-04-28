b0d1db9ef9fdf4080e30cd039483a6812551c317
Robert Collura <rcollura@gmail.com>
2019-04-28 16:46:39 -0700

    Update main.tf

--- exercise_1/modules/main.tf
+++ exercise_1/modules/main.tf
@@ -1,1 +1,13 @@
-
+resource "aws_s3_bucket" "tf-child-module-bucket" {
+  bucket = "${var.bucket_name}"
+  acl = "private"
+  
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name        = "${var.tag_name}"
+    Environment = "${var.env_name}"
+  }
+}
