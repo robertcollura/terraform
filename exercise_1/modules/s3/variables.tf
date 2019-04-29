3d6f25ec968433b5048e2eb5e68655577b230cf1
Robert Collura <rcollura@gmail.com>
2019-04-28 18:33:52 -0700

    Create variables.tf

---
+++ exercise_1/modules/s3/variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  type        = "string"
+  description = "AWS Region required by Terraform AWS Provider"
+  default     = "us-east-1"
+  }
+
+variable "s3_bucket_region" {
+  type        = "string"
+  description = "Region where S3 bucket will be created"
+  default     = "us-east-1"
+  }
+
+variable "s3_bucket_name" {
+  type        = "string"
+  description = "Name of the S3 bucket to be created"
+  default     = "us-east-1"
+  }
+
+variable "tag_env" {
+  type        = "string"
+  description = "Tag for Environment Name"
+  default     = "Training"
+  }
+
+# Variable type 'list'
+
+variable "users" {
+    type    = "list"
+    default = ["root", "user1", "user2"]
+    }
+
+# Variable type 'MAP'
+
+# variable "templates" {
+#    type = "map"
+#    default = {
+#        "template1" = "01000000"
+#        "template2" = "02000000"
+#        "template3" = "03000000"
+#    }
+# }
