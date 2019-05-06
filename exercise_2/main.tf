fd432ea04b572de28773ed73b574e173b569475c
Robert Collura <rcollura@gmail.com>
2019-05-05 20:45:23 -0700

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -3,28 +3,53 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-  bucket = "rc-tf-remote-state-bucket"
-  key = "terraform.tfstate"
-  region = "us-east-1"
+   backend "s3" {
+   bucket = "rc-tf-remote-state-bucket"
+   key = "terraform.tfstate"
+   region = "us-east-1"
   }
 }
 
-# References main.tf configuration in S3 child module
-module "s3_module_same_repo" {
+resource "aws_s3_bucket" "tf-child-module-bucket" {
+  bucket = "${var.s3_bucket_name}"
+  acl = "private"
+  region = "${var.s3_bucket_region}"
+  
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name        = "${var.s3_bucket_name}"
+    Environment = "${var.tag_env}"
+  }
+}
+
+###############################################################
+### Calling a local module in the same repo (relative path) ###
+###############################################################
+
+module "s3_module_local_repo" {
   source = "./modules/s3"
 }
   
-# References main.tf config in a remote private git repo
-# A git ssh key for the private repo is required on the Harness delegate for Terraform to perform git clone
-# https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
+#####################################################
+### Calling a remote module in a private Git repo ###
+#####################################################
 
- # module "s3_private_repo" {
- #  source = "git@github.com:rc-harness/private.git"
- # }
+### NOTE: A ssh key for the private Git repo is required on the Harness delegate for Terraform to download the module
+### NOTE: https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
 
-# References main.tf config in a pulic git repo
+#module "s3_module_private_repo" {
+#  source = "git@github.com:rc-harness/private.git"
+#}
+
+####################################################
+### Calling a remote module in a public Git repo ###
+####################################################
   
- # module "s3_public_repo" {
- #  source = "github.com/rc-harness/public.git"
- # }
+### NOTE: No Git credentials/keys required on the Harness delegate for Terraform to download the module
+  
+#module "s3_module_public_repo" {
+#  source = "github.com/rc-harness/public.git"
+#}
