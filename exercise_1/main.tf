9c1c63aa5263cd53e8bab17f68843c0beeded622
Robert Collura <rcollura@gmail.com>
2019-05-05 20:40:51 -0700

    Update main.tf

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -10,6 +10,21 @@ terraform {
   }
 }
 
+resource "aws_s3_bucket" "tf-child-module-bucket" {
+  bucket = "${var.s3_bucket_name}"
+  acl = "private"
+  region = "${var.s3_bucket_region}"
+  
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name        = "${var.s3_bucket_name}"
+    Environment = "${var.tag_env}"
+  }
+}
+
 ###############################################################
 ### Calling a local module in the same repo (relative path) ###
 ###############################################################
