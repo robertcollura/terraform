b798a5aa8e86f00681dd89e48189630431cb46e7
Robert Collura <rcollura@gmail.com>
2019-06-02 19:01:56 -0700

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -10,7 +10,7 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "tf-child-module-bucket" {
+resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   region = "${var.s3_bucket_region}"
@@ -24,3 +24,4 @@ resource "aws_s3_bucket" "tf-child-module-bucket" {
     Environment = "${var.tag_env}"
   }
 }
+
