15f63f50ee5a451f8b01f34a5de4b256cd98c00c
Robert Collura <rcollura@gmail.com>
2020-01-23 10:32:31 -0800

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = "string"
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "rc-tf-bucket-training-exersise-1"
+  default     = "rc-tf-bucket-training-exercise-1"
   }
 
 variable "tag_env" {
