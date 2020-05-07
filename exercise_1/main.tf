415b9590679711774e9a914c30792c4b66c8e904
Robert Collura <rcollura@gmail.com>
2020-05-07 00:05:56 -0600

    Update main.tf

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -17,6 +17,8 @@ resource "aws_s3_bucket" "tf-root-module-bucket" {
     Name        = "${var.s3_bucket_name}"
     Environment = "${var.tag_env}"
   }
+ }
 module "s3_module_private_repo" {
   source = "git@github.com:rc-harness/private.git"
   }
+  
